d2ee73dc8e8322d5d0639ca8ec2cd43b09a2f1cf
sajan <sajan@tankbot.(none)>
2014-05-19 11:39:49 -0400

    Added event matching files

--- utilities/pubmed_data_experiments/pathogen_search/infer_grid_event_if_keys_in_title.py
+++ utilities/pubmed_data_experiments/pathogen_search/infer_grid_event_if_keys_in_title.py
@@ -1,5 +1,5 @@
-#Script to infer grid event if event keys ( pathogen name, location, year ) 
-#are present in abstracts
+#Script to match grid event metadata ( event, location, year )
+#in title 
 
 import csv
 import re
@@ -7,7 +7,7 @@ from collections import defaultdict
 from collections import Counter
 import argparse
 
-#Dictionary to hold GRID Event Keys
+#Dictionary to hold GRID event keys
 event_term_list = defaultdict(list)
 
 #Initialize GRID event key list
@@ -22,20 +22,21 @@ def createEventTermList():
                 if row[i] != '':
                     event_term_list[key].append(row[i])
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("outputFile",help="Name of output CSV file",type=str)
-    parser.add_argument("thresholdVal",help="Number of matches required to infer GRID event",type=int)	
+    parser.add_argument("thresholdVal",help="Number of matches required to infer GRID event",type=int)
     args = parser.parse_args()
     out_file = outputFile
     threshold = thresholdVal
 
-    #This is being done so that large files could 
+    #This is being done so that large files could
     #be broken down into smaller chunks and parsed. 
     #It could be replaced if an alternative is found.
     filelist = ['pubmed_research_articles']
-    
-    #Call function to create GRID Event dictionary
+
+    #Call function to create GRID event dictionary
     createEventTermList()
 
     num_no_match = 0
@@ -46,44 +47,41 @@ def main():
     #Iterate through list of files
     for file_name in filelist:
         in_file = file_name + ".csv"
-        header = "doi,year,month,article_title,abstract,keywords,journal_title,journal_publisher,journal_volume,journal_issue".split(",")
+        header = "doi,year,month,article_title,abstract,keywords,journal_title,journal_publisher,journal_volume,journal_issue".split(
+            ",")
         with open(in_file, "r") as f:
             csv.field_size_limit(100000000)
             reader = csv.DictReader(f, header)
             for row in reader:
                 total += 1
-		
+
 		#Verify if article has doi. If not, 
-		#then parse next article.
+                #then parse next article.
                 if row['doi'] == "na":
                     num_without_doi += 1
                     continue
 
-                abstract = ""
-
-		#This is done to create uniformity in 
-		#case matching.
-                abstract = row['abstract'].lower()
-
-		#Initialize variables to store matched terms
-                terms_present = defaultdict(list)
+                title = ""
+                title = row['article_title'].lower()
+                
+		#Initialize variable to store matched terms
+		terms_present = defaultdict(list)
                 num_match = [0] * len(event_term_list)
 
-		#Iterate through GRID event terms and search 
-		#for them in abstracts. If present, update 
-		#variables.
+		#Iterate through GRID event terms and search for 
+		#them in titles. If present, update variables.
                 for id_i, terms in event_term_list.items():
                     for val in terms:
                         val = '\\b(' + val.lower() + ')\\b'
-                        match = re.search(val, abstract)
+                        match = re.search(val, title)
                         if match is not None:
                             id_val = int(id_i)
                             num_match[id_val] = num_match[id_val] + 1
                             terms_present[id_i].append(match.group(0))
 
-		#Check for number of matches for each event against 
-		#threshold. If present, write to file.	
-                for x in range(0, len(event_term_list)):
+                #Check for number of matches for each event against
+		#threshold. If present, write to file.
+		for x in range(0, len(event_term_list)):
                     final_text = ""
                     if num_match[x] > threshold:
                         index = str(x)
@@ -92,7 +90,7 @@ def main():
                             + "\"" + event_name + "\"" + "," + "\"" + row['doi'] + "\"" + "," \
                             + "\"" + row['year'] + "\"" + "," \
                             + "\"" + row['month'] + "\"" + "," \
-                            + "\"" + row['article_title'] + "\"" + "," \
+                            + "\"" + title + "\"" + "," \
                             + "\"" + row['journal_title'] + "\"" \
                             + "," + "\""
                         for val in terms_present[index]:
